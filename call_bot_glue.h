c02b073214e54cf8747b86935c42fed9939aa8a2
Doan Tien <tiendc@metechvn.com>
2023-02-22 11:43:17 +0700

    fix build error

--- call_bot_glue.h
+++ call_bot_glue.h
@@ -1,10 +1,11 @@
 #ifndef __CALL_BOT_GLUE_H__
 #define __CALL_BOT_GLUE_H__
+#include "mod_call_bot.h"
 
 switch_status_t call_bot_init();
 switch_status_t call_bot_cleanup();
 switch_status_t call_bot_session_init(switch_core_session_t *session, responseHandler_t responseHandler,
-                                           uint32_t samples_per_second, uint32_t channels, char *lang, int interim, char *bugname, void **ppUserData);
+                                      uint32_t samples_per_second, uint32_t channels, char *lang, int interim, char *bugname, void **ppUserData);
 switch_status_t call_bot_session_cleanup(switch_core_session_t *session, int channelIsClosing, switch_media_bug_t *bug);
 switch_bool_t call_bot_frame(switch_media_bug_t *bug, void *user_data);
 
