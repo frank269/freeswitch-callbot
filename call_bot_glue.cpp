8d374bb25e6f10d26a52c8d21e0624fafa2a558e
Tiendoan <tien2609@gmail.com>
2023-02-23 23:31:36 +0700

    try

--- call_bot_glue.cpp
+++ call_bot_glue.cpp
@@ -348,7 +348,7 @@ static void *SWITCH_THREAD_FUNC grpc_read_thread(switch_thread_t *thread, void *
                 // }
                 // else
                 {
-                    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "grpc_read_thread: write frame to session failed %d!\n", sizeof(bytes_to_play) / sizeof(uint8_t););
+                    switch_log_printf(SWITCH_CHANNEL_LOG, SWITCH_LOG_ERROR, "grpc_read_thread: write frame to session failed %d!\n", sizeof(bytes_to_play) / sizeof(uint8_t));
                 }
             }
             else
