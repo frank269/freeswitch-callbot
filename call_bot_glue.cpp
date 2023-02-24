7875902b106125e279427fcd58e243cdf7c89201
Doan Tien <tiendc@metechvn.com>
2023-02-24 10:31:54 +0700

    fix vector size

--- call_bot_glue.cpp
+++ call_bot_glue.cpp
@@ -24,8 +24,8 @@ typedef struct WAV_HEADER
     uint16_t AudioFormat = 1;              // Audio format 1=PCM,6=mulaw,7=alaw,     257=IBM
                                            // Mu-Law, 258=IBM A-Law, 259=ADPCM
     uint16_t NumOfChan = 1;                // Number of channels 1=Mono 2=Sterio
-    uint32_t SamplesPerSec = 8000;         // Sampling Frequency in Hz
-    uint32_t bytesPerSec = 8000 * 2;       // bytes per second
+    uint32_t SamplesPerSec = 16000;        // Sampling Frequency in Hz
+    uint32_t bytesPerSec = 16000 * 2;      // bytes per second
     uint16_t blockAlign = 2;               // 2=16-bit mono, 4=16-bit stereo
     uint16_t bitsPerSample = 16;           // Number of bits per sample
     /* "data" sub-chunk */
