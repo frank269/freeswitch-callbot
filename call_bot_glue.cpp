88845afcca50edccd0b2d88861bd3b176c79268e
Tiendoan <tien2609@gmail.com>
2023-02-26 20:35:30 +0700

    try add custom event

--- call_bot_glue.cpp
+++ call_bot_glue.cpp
@@ -531,7 +531,7 @@ static void *SWITCH_THREAD_FUNC grpc_read_thread(switch_thread_t *thread, void *
         if (switch_event_create_subclass(&event, SWITCH_EVENT_CUSTOM, "test") == SWITCH_STATUS_SUCCESS)
         {
             // switch_channel_event_set_basic_data(channel, event);
-            switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "text_asr", response->text_asr().c_str());
+            switch_event_add_header_string(event, SWITCH_STACK_BOTTOM, "text_asr", response.text_asr().c_str());
             switch_event_fire(&event);
         }
 
