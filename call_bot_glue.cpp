a41bf70720a4a4881973d433e07ab1145054af7e
tien2609 <tiendc@metechvn.com>
2023-07-11 16:28:19 +0700

    change to ivr broadcast

--- call_bot_glue.cpp
+++ call_bot_glue.cpp
@@ -454,10 +454,10 @@ static switch_status_t play_audio(char *session_id, std::vector<uint8_t> audio_d
     // }
     // return status;
 
-    switch_ivr_stop_displace_session(session, "local_stream://foo");
+    switch_ivr_stop_displace_session(session, "/3eb58bc7-08d6-405b-867c-16417d684f7e.wav");
     // switch_channel_set_variable(channel, "IS_PLAYING", "true");
     // status = switch_ivr_broadcast(session_id, fileName.c_str(), SMF_ECHO_ALEG | SMF_HOLD_BLEG);
-    status = switch_ivr_displace_session(session, "local_stream://foo", 0, "");
+    status = switch_ivr_displace_session(session, "/3eb58bc7-08d6-405b-867c-16417d684f7e.wav", 0, "");
     // if (status != SWITCH_STATUS_SUCCESS)
     // {
     //     switch_log_printf(SWITCH_CHANNEL_SESSION_LOG(session), SWITCH_LOG_WARNING,
